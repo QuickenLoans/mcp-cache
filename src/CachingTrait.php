6018e7f38d95f38fbfba609b5167f6f4cdc677ac
Steve Kluck <stevekluck@quickenloans.com>
2014-11-12 10:03:06 -0500

    Add CachingTrait

---
+++ src/CachingTrait.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * @copyright ©2014 Quicken Loans Inc. All rights reserved. Trade Secret,
+ *    Confidential and Proprietary. Any dissemination outside of Quicken Loans
+ *    is strictly prohibited.
+ */
+
+namespace MCP\Cache;
+
+/**
+ * Provide convenience methods and wrappers for caching in repositories.
+ *
+ * This trait gracefully handles cases where no cache is set.
+ *
+ * NEVER access properties set in traits directly from within the consumer of the trait!
+ *
+ * For example:
+ * Use $this->cache()
+ * Not $this->cache
+ *
+ * @internal
+ */
+trait CachingTrait
+{
+    /**
+     * @var CacheInterface|null
+     */
+    private $cache;
+
+    /**
+     * @var int|null
+     */
+    private $cacheTTL;
+
+    /**
+     * @return CacheInterface|null
+     */
+    private function cache()
+    {
+        return $this->cache;
+    }
+
+    /**
+     * @param string $key
+     * @return mixed|null
+     */
+    private function getFromCache($key)
+    {
+        if (!$this->cache()) {
+            return null;
+        }
+
+        return $this->cache()->get($key);
+    }
+
+    /**
+     * @param string $key
+     * @param mixed $value
+     * @param mixed $ttl
+     * @return null
+     */
+    private function setToCache($key, $value, $ttl = 0)
+    {
+        if (!$this->cache()) {
+            return;
+        }
+
+        $params = func_get_args();
+
+        // Use default TTL if none is provided
+        if ($this->cacheTTL && func_num_args() < 3) {
+            array_push($params, $this->cacheTTL);
+        }
+
+        return call_user_func_array([$this->cache(), 'set'], $params);
+    }
+
+    /**
+     * @param CacheInterface $cache
+     * @return null
+     */
+    public function setCache(CacheInterface $cache)
+    {
+        $this->cache = $cache;
+    }
+
+    /**
+     * @param int $ttl
+     * @return null
+     */
+    public function setCacheTTL($ttl)
+    {
+        $this->cacheTTL = (int) $ttl;
+    }
+}
---
+++ testing/tests/CachingTraitTest.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * @copyright ©2014 Quicken Loans Inc. All rights reserved. Trade Secret,
+ *    Confidential and Proprietary. Any dissemination outside of Quicken Loans
+ *    is strictly prohibited.
+ */
+
+namespace MCP\Cache;
+
+use PHPUnit_Framework_TestCase;
+use MCP\Cache\Testing\Caching;
+use Mockery;
+
+class CachingTraitTest extends PHPUnit_Framework_TestCase
+{
+    public $cache;
+
+    public function setUp()
+    {
+        $this->cache = Mockery::mock('MCP\Cache\CacheInterface');
+    }
+
+    public function testCacheAccessor()
+    {
+        $caching = new Caching;
+
+        $this->assertNull(null, $caching->cache());
+
+        $caching->setCache($this->cache);
+        $this->assertSame($this->cache, $caching->cache());
+    }
+
+    public function testSettingToCacheWithoutCacheSetDoesNotBlowUp()
+    {
+        $caching = new Caching;
+        $actual = $caching->setToCache('key', 'data');
+        $this->assertNull($actual);
+    }
+
+    public function testGettingFromCacheWithoutCacheSetDoesNotBlowUp()
+    {
+        $caching = new Caching;
+        $actual = $caching->getFromCache('key');
+        $this->assertNull($actual);
+    }
+
+    public function testSettingToCacheWithCacheSetCallsCache()
+    {
+        $this->cache
+            ->shouldReceive('set')
+            ->with('key', 'data')
+            ->once();
+
+        $caching = new Caching;
+        $caching->setCache($this->cache);
+
+        $caching->setToCache('key', 'data');
+    }
+
+    public function testGettingFromCacheWithCacheSetCallsCache()
+    {
+        $this->cache
+            ->shouldReceive('get')
+            ->with('key')
+            ->once()
+            ->andReturn('data2');
+
+        $caching = new Caching;
+        $caching->setCache($this->cache);
+
+        $actual = $caching->getFromCache('key');
+        $this->assertSame('data2', $actual);
+    }
+
+    public function testGlobalTTLIsUsedIfNotExplicitlySet()
+    {
+        $this->cache
+            ->shouldReceive('set')
+            ->with('key', 'data', 100)
+            ->once();
+
+        $caching = new Caching;
+        $caching->setCache($this->cache);
+        $caching->setCacheTTL('100');
+
+        $caching->setToCache('key', 'data');
+    }
+
+    public function testGlobalTTLIsIgnoredByLocalTTL()
+    {
+        $this->cache
+            ->shouldReceive('set')
+            ->with('key', 'data', 200)
+            ->once();
+
+        $caching = new Caching;
+        $caching->setCache($this->cache);
+        $caching->setCacheTTL('100');
+
+        $caching->setToCache('key', 'data', 200);
+    }
+}
